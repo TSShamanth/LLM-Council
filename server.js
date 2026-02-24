95e69af7722f37ee820c11f67e416d08386c1c35
tsshamanth <shamantht457@gmail.com>
2026-02-24 12:36:19 +0530

    Deploy

--- server.js
+++ server.js
@@ -391,6 +391,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, '127.0.0.1', () => {
-  console.log(`✅ Server running on http://127.0.0.1:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
